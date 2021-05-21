feb7a3a0f365434977f175d5917cc24f59ffda25
Thomas Dziedzic <thomas@getcensus.com>
2021-05-21 12:00:41 -0500

    update tests

--- tests/operators/test_census.py
+++ tests/operators/test_census.py
@@ -5,5 +5,5 @@ from airflow_provider_census.operators.census import CensusOperator
 class TestCensusOperator:
     @vcr.use_cassette()
     def test_census_operator(self):
-        operator = CensusOperator(sync_id = 2648, task_id = 'census_operator')
+        operator = CensusOperator(sync_id = 0, task_id = 'census_operator')
         operator.execute({})
