d3be6d385d57145570895c933ff3d8d6a65d31cd
Thomas Dziedzic <thomas@getcensus.com>
2021-06-03 14:43:34 -0500

    test timeout scenario

--- tests/operators/test_census.py
+++ tests/operators/test_census.py
@@ -1,5 +1,8 @@
 from airflow_provider_census.operators.census import CensusOperator
 
+from airflow.exceptions import AirflowException, AirflowTaskTimeout
+import pytest
+
 class TestCensusOperator:
     def test_census_operator(self, requests_mock):
         trigger_json = {
@@ -45,3 +48,35 @@ class TestCensusOperator:
 
         operator = CensusOperator(sync_id = 0, wait_seconds = 1, task_id = 'census_operator')
         operator.execute({})
+
+    def test_census_operator_failed_status(self, requests_mock):
+        pass
+
+    def test_census_operator_timed_out(self, requests_mock):
+        trigger_json = {
+            'status': 'success',
+            'data': {
+                'sync_run_id': 0
+            }
+        }
+        requests_mock.post('https://app.getcensus.com/api/v1/syncs/0/trigger', json = trigger_json)
+
+        run_sync_responses = [
+            {
+                'status_code': 200,
+                'json': {
+                    'status': 'success',
+                    'data': {
+                        'status': 'working'
+                    }
+                }
+            }
+        ]
+        requests_mock.register_uri('GET', 'https://app.getcensus.com/api/v1/sync_runs/0', run_sync_responses)
+
+        operator = CensusOperator(sync_id = 0, wait_seconds = 1, timeout_seconds = 1, task_id = 'census_operator')
+
+        with pytest.raises(AirflowTaskTimeout) as excinfo:
+            operator.execute({})
+
+            assert 'Timed out for sync 0 while waiting for sync run 0' == excinfo.value
