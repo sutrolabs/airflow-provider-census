9449a240dd4c495ba0c26c71aa92569002319383
Thomas Dziedzic <thomas@getcensus.com>
2021-06-03 14:47:32 -0500

    test failed task scenario

--- tests/operators/test_census.py
+++ tests/operators/test_census.py
@@ -50,7 +50,43 @@ class TestCensusOperator:
         operator.execute({})
 
     def test_census_operator_failed_status(self, requests_mock):
-        pass
+        trigger_json = {
+            'status': 'success',
+            'data': {
+                'sync_run_id': 0
+            }
+        }
+        requests_mock.post('https://app.getcensus.com/api/v1/syncs/0/trigger', json = trigger_json)
+
+        run_sync_responses = [
+            {
+                'status_code': 200,
+                'json': {
+                    'status': 'success',
+                    'data': {
+                        'status': 'working'
+                    }
+                }
+            },
+            {
+                'status_code': 200,
+                'json': {
+                    'status': 'success',
+                    'data': {
+                        'error_message': 'borked',
+                        'status': 'failed'
+                    }
+                }
+            }
+        ]
+        requests_mock.register_uri('GET', 'https://app.getcensus.com/api/v1/sync_runs/0', run_sync_responses)
+
+        operator = CensusOperator(sync_id = 0, wait_seconds = 1, task_id = 'census_operator')
+
+        with pytest.raises(AirflowException) as excinfo:
+            operator.execute({})
+
+            assert 'Sync 0 failed for sync run 0 with error "borked"' == excinfo.value
 
     def test_census_operator_timed_out(self, requests_mock):
         trigger_json = {
